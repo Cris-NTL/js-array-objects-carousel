44c7e408ce41a341c2ae0762ec734683e2e7cc83
Cris-NTL <natolicristian92@gmail.com>
2023-03-23 17:26:08 +0100

    Add: custom style, buttons

---
+++ js/script.js
@@ -0,0 +1,111 @@
+// array of objects
+const arrayOfImages = [
+    {
+        image: 'img/01.webp',
+        title: 'Marvel\'s Spiderman Miles Morale',
+        text: 'Experience the rise of Miles Morales as the new hero masters incredible, explosive new powers to become his own Spider-Man',
+    }, {
+        image: 'img/02.webp',
+        title: 'Ratchet & Clank: Rift Apart',
+        text: 'Go dimension-hopping with Ratchet and Clank as they take on an evil emperor from another reality',
+    }, {
+        image: 'img/03.webp',
+        title: 'Fortnite',
+        text: "Grab all of your friends and drop into Epic Games Fortnite, a massive 100 - player face - off that combines looting, crafting, shootouts and chaos",
+    }, {
+        image: 'img/04.webp',
+        title: 'Stray',
+        text: 'Lost, injured and alone, a stray cat must untangle an ancient mystery to escape a long-forgotten city',
+    }, {
+        image: 'img/05.webp',
+        title: "Marvel's Avengers",
+        text: 'Marvel\'s Avengers is an epic, third-person, action-adventure game that combines an original, cinematic story with single-player and co-operative gameplay',
+    }
+];
+
+// container DOM
+const imagesContainer = document.querySelector('.slider-items');
+
+// display array's elements on DOM
+arrayOfImages.forEach((element) => {
+
+    let item = ` <div class="item">
+                    <img src="${element.image}">
+                    <div class="text">
+                        <h3 class="title">${element.title}</h3>
+                        <h6 class="subTitle">${element.text}</h6>
+                    </div>
+                </div>`
+
+    imagesContainer.innerHTML += item;
+});
+
+// add "active" class to the first element
+const itemOfArray = document.getElementsByClassName('item');
+let activeItem = 0;
+itemOfArray[activeItem].classList.add('active');
+
+// buttons management
+const upBtn = document.querySelector(".up-btn");
+const downBtn = document.querySelector(".down-btn");
+const pauseBtn = document.querySelector(".pause-btn");
+const repeatBtn = document.querySelector(".repeat-btn");
+
+// hide the up button
+// upBtn.style.display = "none";
+
+// click function for up-button
+upBtn.addEventListener('click', function () {
+    downBtn.style.display = "block";
+    if (activeItem > 0) {
+        itemOfArray[activeItem].classList.remove('active');
+        activeItem--;
+        itemOfArray[activeItem].classList.add('active');
+    }
+    // // hide the up button if it reaches the first item
+    // if (activeItem === 0) {
+    //     upBtn.style.display = "none";
+    // }
+})
+
+// click function for down-button
+downBtn.addEventListener('click', function () {
+    upBtn.style.display = "block";
+    if (activeItem < (arrayOfImages.length - 1)) {
+        itemOfArray[activeItem].classList.remove("active");
+        activeItem++;
+        itemOfArray[activeItem].classList.add("active");
+    }
+    // hide the down button if it reaches the last item
+    // if (activeItem === arrayOfImages.length - 1) {
+    //     downBtn.style.display = "none";
+    // }
+});
+
+// function setInterval
+setInterval(function () {
+    if (activeItem < (arrayOfImages.length - 1)) {
+        itemOfArray[activeItem].classList.remove("active");
+        activeItem++;
+        itemOfArray[activeItem].classList.add("active");
+    } else {
+        itemOfArray[activeItem].classList.remove("active");
+        activeItem = 0;
+        itemOfArray[activeItem].classList.add("active");
+    }
+    // // hide the up button if it reaches the first item
+    // if (activeItem === 0) {
+    //     upBtn.style.display = "none";
+    // } else {
+    //     upBtn.style.display = "block";
+    // }
+    // // hide the down button if it reaches the last item
+    // if (activeItem === arrayOfImages.length - 1) {
+    //     downBtn.style.display = "none";
+    // } else {
+    //     downBtn.style.display = "block";
+    // }
+}, 3000);
+
+
+
